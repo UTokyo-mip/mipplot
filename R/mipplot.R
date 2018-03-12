395ecd950fb00567ba2df04c8bc771934afa0492
Akimitsu Inoue <inoue.akimitsu.github@chino-js.com>
2018-03-12 22:41:56 +0900

    add packaging related files

---
+++ R/mipplot.R
@@ -0,0 +1,9 @@
+#' mipplot
+#'
+#' Package contains generic functions to produce area/bar/box/line plots of data following IAMC submission format.
+#'
+#' @name mipplot
+#' @docType package
+#' @import ggplot2 data.table openxlsx stringr reshape2 shiny stringi plyr
+#'
+NULL
