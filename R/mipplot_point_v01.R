f40733dc73359d42971e56b3c6357af34fff2023
silva-diego <silva-herran@iges.or.jp>
2018-03-15 12:15:34 +0900

    Plot point with aesthetics (color, shape) as arguments.

--- R/mipplot_point_v01.R
+++ R/mipplot_point_v01.R
@@ -10,45 +10,47 @@
 #' @example mipplot_point(AR5_Sample_data)
 #' @export p_list1
 
-mipplot_point <- function(D,region=levels(D$region),variable=levels(D$variable), target_year=levels(D$period),PRINT_OUT=F,DEBUG=T){
-
-
+mipplot_point <- function(D,region=levels(D$region),variable=levels(D$variable), target_year=levels(D$period),
+                          colorby="model", shapeby="model",
+                          PRINT_OUT=F,DEBUG=T){
+  
   p_list1 <- list()
-
+  
   for(r in levels(as.factor(region))){
-
+    
     for (v in levels(as.factor(variable))){
-
+      
       for(ty in levels(as.factor(target_year))){
-
-
+        
+        
         D_sub <- D[D$region==r & D$variable==v & D$period==ty, ]
         ## removing NA ensures lines are connected
         D_sub <- D_sub[!is.na(D_sub$value),]
-
+        
         if(nrow(D_sub)==0){next()}    # Skip iteration if data is empty for the selected scope (region/variable).
-
+        
         ##Title
         tt1 <- paste("region:",r,",  period:",ty, sep="")
         tt2 <- paste("variable:",as.character(v), sep="")
         tt3 <- paste(' [',D_sub$unit[1],']', sep="")
-
+        
         ## Box plots: using scenario
         p_Out1 <- ggplot2::ggplot(data=D_sub,ggplot2::aes(x=scenario, y=value))+
-          ggplot2::geom_point(ggplot2::aes(color=model, shape=model),size=5) +
+          #ggplot2::geom_point(ggplot2::aes(color=model, shape=model),size=5) +
+          ggplot2::geom_point(ggplot2::aes_(color=as.name(colorby), shape=as.name(shapeby), fill=as.name(colorby)),size=5) +
+          ggplot2::scale_shape_manual(values=seq(0,10))+
           ggplot2::labs(title=tt1, subtitle=tt2, y=tt3)+
           ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90))
-
-
+        
+        
         p_Out1 <- p_Out1 + ggplot2::theme(text=ggplot2::element_text(size = 20))
-
+        
         ## STORE PLOTS TO LIST
         p_list1[[length(p_list1)+1]] <- p_Out1
       }
     }
   }
-
-
+  
   if (PRINT_OUT == TRUE){
     ## Open printing device.
     filename <-sprintf("../data_output/JpMIP_plots_point_%s.pdf", format(Sys.time(), "%Y_%m%d"))
