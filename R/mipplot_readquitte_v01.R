995554be9e0f3cbfe50dc8dd2809e4ef4a75f657
Akimitsu Inoue <inoue.akimitsu.github@chino-js.com>
2018-03-28 21:41:38 +0900

    add simple exception handling in mipplot_readquitte

--- R/mipplot_readquitte_v01.R
+++ R/mipplot_readquitte_v01.R
@@ -22,7 +22,17 @@ mipplot_readquitte <- function(filename=NULL, sep=",", interactive=FALSE, DEBUG=
     sep <- readline()
   }
 
-  return(quitte::read.quitte(filename, sep = sep))
+  loaded_quitte = quitte::read.quitte(filename, sep = sep)
+
+  if (nrow(loaded_quitte) == 0) {
+    stop("No rows available in the given quitte file.")
+  }
+
+  if (!("quitte" %in% class(loaded_quitte))) {
+    stop("This is not quitte format file.")
+  }
+
+  return(loaded_quitte)
 }
 
 #END
