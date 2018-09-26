0b7947e08184acea87b0570c63ff82098e5581da
silva-diego <silva-diego@users.noreply.github.com>
2018-09-26 17:17:07 +0900

    Update read_and_preprocess_data.R
    Changed pointer to updated files of sample data and rule table.

--- data-raw/read_and_preprocess_data.R
+++ data-raw/read_and_preprocess_data.R
@@ -1,10 +1,10 @@
 # Run this script in `mipplot/data-raw` directory.
 
-ar5_db_sample09_Wang <- mipplot_readquitte("ar5_db_sample09_Wang.csv")
-devtools::use_data(ar5_db_sample09_Wang, overwrite = TRUE)
+ar5_db_sample_data <- mipplot_readquitte("ar5_db_sample09_EMF_v01.csv")
+devtools::use_data(ar5_db_sample_data, overwrite = TRUE)
 
-ar5_db_rule_table_v09_Wang <- read.csv("ar5_db_rule_table_v09_Wang.csv")
-devtools::use_data(ar5_db_rule_table_v09_Wang, overwrite = TRUE)
+ar5_db_rule_table <- read.csv("ar5_db_rule_table_v09.csv")
+devtools::use_data(ar5_db_rule_table, overwrite = TRUE)
 
 ar5_db_rule_table_v09_wo_id <- read.csv(
   "ar5_db_rule_table_v09-wo_id.csv", header = TRUE)
