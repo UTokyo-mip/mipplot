a5ef293da5b10b14c1218f65387e7a15248fb1e3
Akimitsu Inoue <inoue.akimitsu.github@chino-js.com>
2018-10-15 21:49:03 +0900

    Add default color palette as a static data

--- data-raw/read_and_preprocess_data.R
+++ data-raw/read_and_preprocess_data.R
@@ -6,3 +6,8 @@ devtools::use_data(ar5_db_sample_data, overwrite = TRUE)
 ar5_db_rule_table_v09_wo_id <- read.csv(
   "ar5_db_rule_table_v09.csv", header = TRUE, stringsAsFactors = FALSE)
 devtools::use_data(ar5_db_rule_table_v09_wo_id, overwrite = TRUE)
+
+# Generate default color pallete
+mipplot_default_color_palette <-
+  mipplot_generate_color_mapper(ar5_db_rule_table_v09_wo_id)
+devtools::use_data(mipplot_default_color_palette, overwrite = TRUE)
