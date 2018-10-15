e60e11ee8e6a62f7b35871696a2cc19b2de580aa
Akimitsu Inoue <inoue.akimitsu.github@chino-js.com>
2018-10-15 20:28:00 +0900

    Add stringsAsFactors = FALSE to data-raw/read_and_preprocess_data.R

--- data-raw/read_and_preprocess_data.R
+++ data-raw/read_and_preprocess_data.R
@@ -4,5 +4,5 @@ ar5_db_sample_data <- mipplot_readquitte("ar5_db_sample09_EMF_v01.csv")
 devtools::use_data(ar5_db_sample_data, overwrite = TRUE)
 
 ar5_db_rule_table_v09_wo_id <- read.csv(
-  "ar5_db_rule_table_v09.csv", header = TRUE)
+  "ar5_db_rule_table_v09.csv", header = TRUE, stringsAsFactors = FALSE)
 devtools::use_data(ar5_db_rule_table_v09_wo_id, overwrite = TRUE)
