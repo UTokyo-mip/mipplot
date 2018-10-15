1aa0b1f42c84dbb1c2a40257b888d7f6011abab0
Akimitsu Inoue <inoue.akimitsu.github@chino-js.com>
2018-10-15 20:34:52 +0900

    Add stringsAsFactor in read.csv in mipplot_read_ruletab.R

--- R/mipplot_read_ruletab.R
+++ R/mipplot_read_ruletab.R
@@ -6,12 +6,13 @@
 #' @description Read table of additivity rule and adds column with id number.
 #' @param R_without_id Path to a file containing data of additivity rule.
 #' @return A dataframe of additivity rule ("ID, Left_side, Right_side")
-#' @example mipplot_readquitte(ar5_db_rule_table_v09_wo_id)
-#' @export empty
+#' @examples
+#' mipplot_readquitte(ar5_db_rule_table_v09_wo_id)
+#' @export mipplot_read_ruletab
 
 mipplot_read_ruletab <- function(R_without_id) {
 
-  R_without_id <- read.csv(R_without_id)
+  R_without_id <- read.csv(R_without_id, stringsAsFactors = FALSE)
 
   # Stop when input data is not data.frame.
   if (!("data.frame" %in% class(R_without_id)))
@@ -37,7 +38,13 @@ mipplot_read_ruletab <- function(R_without_id) {
     R_without_id$Rule_ID[i] <- current_ID
   }
 
-  R_with_id <- R_without_id[, c(3, 1, 2)]
+  # If the dataframe includes column of Color_code,
+  # the result of this function includes the column.
+  if ("Color_code" %in% colnames(R_without_id)) {
+    R_with_id <- R_without_id[, c("Rule_ID", "Left_side", "Right_side", "Color_code")]
+  } else {
+    R_with_id <- R_without_id[, c("Rule_ID", "Left_side", "Right_side")]
+  }
 
   return(R_with_id)
 }
