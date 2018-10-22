748141199be78e3517fc84852449195be55e13ca
Akimitsu INOUE <inoue.akimitsu.github@chino-js.com>
2018-10-22 17:36:47 +0900

    Add documentations of sample datasets.
    Write some dummy text of datasets following this instruction:
    http://r-pkgs.had.co.nz/data.html

---
+++ R/doc_ar5_db_sample_data.R
@@ -0,0 +1,16 @@
+#' Sample Dataset
+#'
+#' A dataset containing AR5 ...
+#'
+#' @format A tibble data.table with 25240 rows and 7 variables:
+#' \describe{
+#'   \item{model}{model, categorical}
+#'   \item{scenario}{scenario, categorical}
+#'   \item{region}{region, ASIA, OECD90 or World}
+#'   \item{variable}{...}
+#'   \item{unit}{...}
+#'   \item{period}{year}
+#'   \item{value}{value}
+#' }
+#' @source \url{http://.../}
+"ar5_db_sample_data"
