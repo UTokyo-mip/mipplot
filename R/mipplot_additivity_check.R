ed3b9c7cfe4a7517ca213d430c0a55b9a12ca9b2
inoueakimitsu-cjs <inoue.akimitsu.github@chino-js.com>
2018-12-05 19:03:05 +0900

    Add prototype of additivity_check function.

---
+++ R/mipplot_additivity_check.R
@@ -0,0 +1,203 @@
+additivity_check <- function(D, R, max_n_plots = Inf) {
+
+  # Compute maximum of absolute relative deviation for all combination.
+  max_abs_relative_deviation_table <- get_max_abs_relative_deviation_table(D, R)
+
+  # Sort combinations in descending order of maximum of absolute relative deviation.
+  sorted_max_abs_relative_deviation_table <-
+    max_abs_relative_deviation_table %>% arrange(desc(max_abs_relative_deviation))
+
+  # Limited conditions will be plotted.
+  n_plots <- min(max_n_plots, nrow(sorted_max_abs_relative_deviation_table))
+
+  # A container of ggplot2 objects
+  p_list = list()
+
+  # Draw area plots for each conditions
+  for (i_condition in 1:n_plots) {
+
+    # Extract ith condition
+    this_condition <-
+      sorted_max_abs_relative_deviation_table[i_condition, ]
+
+    # Draw plot
+    this_plot <- area_plot_of_specific_rule_id(D, R,
+                              region = as.character(this_condition$region),
+                              scenario = as.character(this_condition$scenario),
+                              rule_ids = as.character(this_condition$rule_id))
+
+    # Append new plot to the container
+    p_list[[length(p_list) + 1]] <- this_plot[[1]]
+
+  }
+
+  return(p_list)
+}
+
+get_max_abs_relative_deviation_table <- function(D, R) {
+
+  result <- NULL
+
+  for (i in levels(as.factor(R$Rule_ID))){
+
+    for (r in levels(as.factor(D$region))){
+
+      for (s in levels(as.factor(D$scenario))){
+
+        Var_set <- R[R$Rule_ID == i, ]
+
+        ## Select data
+        D_LHS <- D[D$region == r & D$scenario == s &
+                     D$variable %in% Var_set$Left_side, ]
+
+        D_RHS <- D[D$region == r & D$scenario == s &
+                     D$variable %in% Var_set$Right_side, ]
+
+        # Common Part of Var-name
+        var_common_name <- Var_set[1, 2]
+
+        # Change name of variable by removing
+        # common part from aggregated vairable (LHS).
+        D_RHS$variable <- factor(
+          gsub(paste(var_common_name, "|", sep = ""),"", D_RHS$variable, fixed = T))
+
+        ## Generate plots only if data is available for a given scenario.
+        if (nrow(na.omit(D_RHS[D_RHS$scenario == s, ])) > 0) {
+
+          this_abs_relative_deviation_table <- full_join(
+            D_RHS %>% group_by(model, period) %>% summarize(rhs_sum_value = sum(value)),
+              D_LHS, by = c('model', 'period')) %>% mutate(
+                abs_relative_deviation =
+                  abs((rhs_sum_value - value) / value)) %>%
+            mutate(rule_id = i) # add rule id column
+
+          if (is.null(result)) {
+            result <- this_abs_relative_deviation_table
+          } else {
+            result <- bind_rows(result, this_abs_relative_deviation_table)
+          }
+
+          ## Title
+          tt1 <- paste("region:", r, ",  scenario:", s, sep = "")
+          tt2 <- paste("variable:", as.character(Var_set[1, 2]), sep = "")
+          tt3 <- paste(" [", D_RHS$unit[1], "]", sep = "")
+          print(paste(tt1, tt2, tt3))
+
+        }
+      }
+    }
+  }
+
+  return(
+    result %>% dplyr::group_by(rule_id, variable, region, scenario, model) %>%
+      dplyr::summarize(max_abs_relative_deviation = max(abs_relative_deviation, na.rm = TRUE)) %>%
+      dplyr::ungroup())
+}
+
+area_plot_of_specific_rule_id <- function(
+  D, R, region=levels(D$region), scenario=levels(D$scenario),
+  facet_x=NULL, facet_y=NULL, PRINT_OUT=F, DEBUG=T, fontsize=20,
+  color_code_specify=T, rule_ids=NULL) {
+
+  p_list1 <- list()
+
+  for (i in rule_ids){
+
+    for (r in levels(as.factor(region))){
+
+      for (s in levels(as.factor(scenario))){
+
+        Var_set <- R[R$Rule_ID == i, ]
+
+        ## SELECT DATA
+        D_LHS <- D[D$region == r & D$scenario == s &
+                     D$variable %in% Var_set$Left_side, ]
+
+        D_RHS <- D[D$region == r & D$scenario == s &
+                     D$variable %in% Var_set$Right_side, ]
+
+        # Renaming levels of a factor
+        # http://www.cookbook-r.com/Manipulating_data/Renaming_levels_of_a_factor/
+
+        # Common Part of Var-name
+        var_common_name <- Var_set[1, 2]
+
+        # if color palette isn't specified or color_code column isn't included,
+        # default color palette is applied.
+        # This color_map is used to sort variable names too.
+        if (color_code_specify == FALSE || !("Color_code" %in% colnames(R))) {
+          color_mapper <- mipplot_default_color_palette
+        } else {
+          # otherwise, generate palette.
+          color_mapper <- mipplot_generate_color_mapper(R)
+        }
+
+        ## Title
+        tt1 <- paste("region:", r, ",  scenario:", s, sep = "")
+        tt2 <- paste("variable:", as.character(Var_set[1, 2]), sep = "")
+        tt3 <- paste(" [", D_RHS$unit[1], "]", sep = "")
+
+        # Change name of variable by removing
+        # common part from aggregated vairable (LHS).
+        D_RHS$variable <- factor(
+          gsub(paste(var_common_name, "|", sep = ""),"", D_RHS$variable, fixed = T),
+          levels = rev(names(color_mapper[[var_common_name]])))
+
+        ## Generate plots only if data is available for a given scenario.
+        if (nrow(na.omit(D_RHS[D_RHS$scenario == s, ])) > 0) {
+
+          ### FACET DOES NOT WORK IN NO DATA EXISTS.
+          p_Out1 <-
+            ggplot2::ggplot() +
+            ggplot2::geom_area(
+              data = na.omit(D_RHS),
+              ggplot2::aes(x = period, y = value, fill = variable),
+              position = "stack")
+
+          p_Out1 <- p_Out1 +
+            ggplot2::geom_line(
+              data = na.omit(D_LHS),
+              ggplot2::aes(x = period, y = value), size = 2)
+
+          # Define plot titles and axes labels.
+          p_Out1 <- p_Out1 +
+            ggplot2::labs(title = tt1, subtitle = tt2, y = tt3)
+
+          # Remove legend title.
+          # p_Out1 <- p_Out1 + ggplot2::theme(legend.title=tt_legend)
+
+          if (!is.null(facet_x) & !is.null(facet_y)) {
+
+            facet_by <- paste(facet_y, facet_x, sep = "~")
+            p_Out1 <- p_Out1 + ggplot2::facet_grid(facet_by)
+
+          } else {
+
+            p_Out1 <- p_Out1 + ggplot2::facet_wrap(~model)
+          }
+
+          p_Out1 <- p_Out1 + ggplot2::theme(
+            text = ggplot2::element_text(size = fontsize))
+
+          # apply color palette.
+          if (!is.null(color_mapper[[var_common_name]])) {
+            new_mapper <- color_mapper[[var_common_name]]
+            p_Out1 <- p_Out1 + ggplot2::scale_fill_manual(values=new_mapper)
+          }
+
+          p_list1[[length(p_list1) + 1]] <- p_Out1
+
+        }
+      }
+    }
+  }
+
+  if (PRINT_OUT == TRUE) {
+
+    mipplot_print_pdf(p_list1, filelabel = "area")
+
+  }
+
+  return(p_list1)
+}
+
