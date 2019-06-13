2cfe77fcf7a99c8df4ccd4b5b93666bb9795148a
inoueakimitsu-cjs <inoue.akimitsu.github@chino-js.com>
2019-06-13 16:46:03 +0900

    Refactor mipplot_interactive_line

--- R/mipplot_interactive_line.R
+++ R/mipplot_interactive_line.R
@@ -12,6 +12,7 @@
 mipplot_interactive_line <- function(D) {
 
   library(shiny)
+  library(shinyWidgets)
 
   region_list <- levels(D$region)
   var_list <- levels(D$variable)
@@ -35,29 +36,68 @@ mipplot_interactive_line <- function(D) {
                     list(`variable` = var_list)
                     ),
 
-        checkboxGroupInput("model", "model:",
-                           choiceNames = get_model_name_list(D),
-                           choiceValues = get_model_name_list(D),
-                           # the default model is a first appeared model in D
-                           selected = get_model_name_list(D)[1]
-                           ),
+        # TODO:
+        # The code for the user interface of previous version
+        # are commented out.
+        #
+        # I wonder number of models are not so big to
+        # user shinyWidgets::pickerInput instead of
+        # checkboxGroupInput.
+        #
+        # if new experimental user interface are employed,
+        # please delete these comments.
 
-        checkboxGroupInput("scenario", "scenario:",
-                           choiceNames = get_scenario_name_list(D),
-                           choiceValues = get_scenario_name_list(D),
-                           # the default scenario is a first appeared scenario in D
-                           selected = get_scenario_name_list(D)[1]
-                           ),
+        # checkboxGroupInput("model", "model:",
+        #                    choiceNames = get_model_name_list(D),
+        #                    choiceValues = get_model_name_list(D),
+        #                    # the default model is a first appeared model in D
+        #                    selected = get_model_name_list(D)[1]
+        #                    ),
 
-        selectInput("period_start", "period_start:",
-                    list(`period` = period_list),
-                    selected = 2005
-                    ),
+        shinyWidgets::pickerInput("model",
+                    label = "model:",
+                    choices = get_model_name_list(D),
+                    selected = get_model_name_list(D)[1],
+                    multiple = TRUE,
+                    options = list(
+                      `actions-box` = TRUE
+                    )),
 
-        selectInput("period_end", "period_end:",
-                    list(`period` = period_list),
-                    selected = 2050
-                    )
+        shinyWidgets::pickerInput("scenario",
+                    label = "scenario:",
+                    choices = get_scenario_name_list(D),
+                    selected = get_scenario_name_list(D)[1],
+                    multiple = TRUE,
+                    options = list(
+                      `actions-box` = TRUE
+                    )),
+
+        # TODO:
+        # previous user interfaces are commented out.
+        # if new experimental user interfaces are employed,
+        # please delete these comments.
+
+        # selectInput("period_start", "period_start:",
+        #             list(`period` = period_list),
+        #             selected = 2005
+        #             ),
+
+        # selectInput("period_end", "period_end:",
+        #             list(`period` = period_list),
+        #             selected = 2050
+        #             ),
+
+        shinyWidgets::sliderTextInput(
+          inputId = "period",
+          label = "period:",
+          choices = period_list,
+          selected = c(head(period_list, n = 1),
+                       tail(period_list, n = 1))
+        ),
+
+        # To disable automatic re-draw,
+        # we only have to includes submitButton.
+        submitButton(text = "Apply Changes", icon = NULL, width = NULL)
         ),
 
 
@@ -69,10 +109,27 @@ mipplot_interactive_line <- function(D) {
   server <- function(input, output) {
 
     output$line_plot <- renderPlot({
-      mipplot_line(D,variable = input$variable, #model = input$model,
-                        scenario = input$scenario
-                        #period = seq(input$period_start,input$period_end,5)
-                   )
+
+      # Since mipplot_line() function has no arguments to filter
+      # models and periods to be plotted,
+      # manually narrow the records in D
+      # before entering the mipplot_line() function.
+      D_subset = D %>%
+
+        # filter model
+        dplyr::filter( model %in% input$model ) %>%
+
+        # filter period
+        dplyr::filter(input$period[1] <= period) %>%
+        dplyr::filter(period <= input$period[2])
+        # dplyr::filter(input$period_start <= period) %>%
+        # dplyr::filter(period <= input$period_end)
+
+      # plot D_subset instead of D.
+      mipplot_line(D_subset,
+                   variable = input$variable,
+                   scenario = input$scenario,
+                   region = input$region)
     },
     height = 400, width = 600
     )
@@ -91,7 +148,7 @@ mipplot_interactive_line <- function(D) {
 #' c("AIM-Enduse 12.1", "GCAM 3.0", "IMAGE 2.4" )
 #' @param D A quitte format dataframe of IAMC data to produce garph.
 #' @examples
-#' @dontrun{
+#' \donttest{
 #' get_model_name_list(ar5_db_sample_data)
 #' }
 get_model_name_list <- function(D) {
@@ -104,7 +161,7 @@ get_model_name_list <- function(D) {
 #' c("EMF27-450-Conv", "EMF27-450-FullTech", "EMF27-450-NoCCS", "EMF27-450-NucOff")
 #' @param D A quitte format dataframe of IAMC data to produce garph.
 #' @examples
-#' @dontrun{
+#' \donttest{
 #' get_scenario_name_list(ar5_db_sample_data)
 #' }
 get_scenario_name_list <- function(D) {
