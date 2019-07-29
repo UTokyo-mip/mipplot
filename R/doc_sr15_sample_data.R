b6c07500bb4660c865eb5a0cfa4f36eb84966f96
inoueakimitsu-cjs <inoue.akimitsu.github@chino-js.com>
2019-07-29 12:49:26 +0900

    Added sample data consisting of a subset of SR1.5.

---
+++ R/doc_sr15_sample_data.R
@@ -0,0 +1,16 @@
+#' Sample Dataset
+#'
+#' A sample dataset of IAMC format
+#'
+#' @format A tibble data.table with N rows and M variables:
+#' \describe{
+#'   \item{model}{model, categorical}
+#'   \item{scenario}{scenario, categorical}
+#'   \item{region}{region, ASIA, OECD90 or World}
+#'   \item{variable}{the name of simulated variable that changes over time}
+#'   \item{unit}{unit of a variable}
+#'   \item{period}{year}
+#'   \item{value}{the value of a variable}
+#' }
+#' @source \url{}
+"sr15_sample_data"
