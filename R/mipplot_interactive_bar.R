54a566b2a9a10445541df6e0b9bf76b1d8b01992
Akimitsu Inoue <inoue.akimitsu.github@chino-js.com>
2022-02-01 14:17:37 +0900

    fix: replace inefficient line with unique()

--- R/mipplot_interactive_bar.R
+++ R/mipplot_interactive_bar.R
@@ -34,7 +34,7 @@ mipplot_interactive_bar <- function(D, R, language = "en") {
   var_list <- levels(D$variable)
   model_list <- levels(D$model)
   scenario_list <- levels(D$scenario)
-  period_list <- levels(as.factor(D$period))
+  period_list <- unique(D$period)
 
   # get variable-group-name list
   variable_group_name_list <- get_variable_group_name_list(R)
